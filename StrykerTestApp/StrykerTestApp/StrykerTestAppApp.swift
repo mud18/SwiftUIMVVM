e14e74b1f4e28d282defdeb1847408cdb5d2e33a
Mudit Jain <mudit.jain@renishaw.com>
2023-12-09 13:38:46 +0530

    Add: Initial project created

---
+++ StrykerTestApp/StrykerTestApp/StrykerTestAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  StrykerTestAppApp.swift
+//  StrykerTestApp
+//
+//  Created by Mudit Jain on 09/12/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct StrykerTestAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ StrykerTestApp/StrykerTestAppUITests/StrykerTestAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  StrykerTestAppUITestsLaunchTests.swift
+//  StrykerTestAppUITests
+//
+//  Created by Mudit Jain on 09/12/23.
+//
+
+import XCTest
+
+final class StrykerTestAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
